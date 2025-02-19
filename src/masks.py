13bdf6a659e11d987851b9d61a10fcb30692fba2
Павел Зайцев <sirzaitsev@yandex.ru>
2025-02-19 06:33:40 +0300

    new commit

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+    masks = card_number[:6] + " " + card_number[6:8] + "** **** " + card_number[-4:]
+    return masks
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    masks = "**" + account_number[-4:]
+    return masks
