57af6611cea23510eabeaa22c30f09de490a679e
Павел Зайцев <sirzaitsev@yandex.ru>
2025-02-21 06:08:18 +0300

    two commit

---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+
+def mask_account_card(input_string: str) -> str:
+    """Функция принимает строку с типом и номером карты или счета, возвращает замаскированный номер."""
+    # Разделение входной строки на название и номер
+    parts = input_string.split()
+    card_or_account_type = " ".join(parts[:-1])
+    number = parts[-1]
+
+    if card_or_account_type.startswith("Счет"):
+        masked_number = get_mask_account(number)
+    else:
+        masked_number = get_mask_card_number(number)
+
+    return f"{card_or_account_type} {masked_number}"
+
+
+def get_date(date: str) -> str:
+    date_object = datetime.fromisoformat(date)
+    formatted_date = date_object.strftime("%d.%m.%Y")
+    return formatted_date
+
