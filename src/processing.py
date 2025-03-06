b805c3a4a1407fc2456c095146918b916f843ad7
Павел Зайцев <sirzaitsev@yandex.ru>
2025-03-06 07:32:32 +0300

    1 def

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
-def filter_by_state(state):
-    pass
-
-
-def sort_by_date():
-    pass
+def filter_by_state(transactions: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Принимает список словарей и значение для ключа state"""
+    filtered_transactions = []
+    for transaction in transactions:
+        if transaction.get("state") == state:
+            filtered_transactions.append(transaction)
+    return filtered_transactions
