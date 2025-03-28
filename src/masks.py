d2be58b18f4d5b3d2abc4d80565482c8a30a5809
Павел Зайцев <sirzaitsev@yandex.ru>
2025-03-28 06:01:30 +0300

    test_processing

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
+    if len(card_number) != 16:
+        raise ValueError("Неверная длина номера карты. Ожидалось 16 символов.")
     masks = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
     return masks
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
+    if len(account_number) != 20:
+        raise ValueError("Неверная длина номера счета. Ожидалось 20 символов.")
     masks = "**" + account_number[-4:]
     return masks
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -5,6 +5,16 @@ from src.masks import get_mask_card_number, get_mask_account
 def test_get_mask_card_number():
     assert get_mask_card_number("1234567890123456") == "1234 56** **** 3456"
 
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number("987654321012345")
+
+    assert str(exc_info.value) == "Неверная длина номера карты. Ожидалось 16 символов."
+
 
 def test_get_mask_account():
     assert get_mask_account('73654108430135874305') == '**4305'
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account("7365410843013587430")
+
+    assert str(exc_info.value) == "Неверная длина номера счета. Ожидалось 20 символов."
