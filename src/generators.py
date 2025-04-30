85a68769578c8be69b5611d091f8e0c64552bf2c
Павел Зайцев <sirzaitsev@yandex.ru>
2025-04-30 08:31:29 +0300

    filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,12 @@
-def filter_by_currency():
-    pass
+def filter_by_currency(transactions, currency_code):
+    """
+    Генератор, возвращающий транзакции, где код валюты соответствует currency_code.
+
+    """
+    for transaction in transactions:
+        currency = transaction.get("operationAmount", {}).get("currency", {}).get("code")
+        if currency == currency_code:
+            yield transaction
 
 
 def transaction_descriptions():
