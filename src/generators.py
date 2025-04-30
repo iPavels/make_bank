d54e9280b61fa8f6ef9ed56d01432be554b80067
Павел Зайцев <sirzaitsev@yandex.ru>
2025-04-30 15:02:19 +0300

    3 def

--- src/generators.py
+++ src/generators.py
@@ -9,9 +9,18 @@ def filter_by_currency(transactions, currency_code):
             yield transaction
 
 
-def transaction_descriptions():
-    pass
+def transaction_descriptions(transactions):
+    """Генератор, возвращающий по одному описанию из каждой транзакции"""
+    for transaction in transactions:
+        description = transaction.get("description")
+        if description:
+            yield description
 
 
-def card_number_generator():
-    pass
+def card_number_generator(start, stop):
+    """Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
+    for number in range(start, stop + 1):
+        card_number = f"{number:016d}"
+        formatted = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+        yield formatted
+
