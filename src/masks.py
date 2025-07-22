3c2b365426b75b7a52e16dadbf073d42fc8adb74
Павел Зайцев <sirzaitsev@yandex.ru>
2025-07-22 14:53:05 +0300

    logs mask and utils

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,44 @@
+import logging
+import os
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+
+os.makedirs("logs", exist_ok=True)
+
+
+file_handler = logging.FileHandler("logs/masks.log", mode="w", encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+
+
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+file_handler.setFormatter(file_formatter)
+
+
+if not logger.handlers:
+    logger.addHandler(file_handler)
+    logger.propagate = False
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
     if len(card_number) != 16:
+        logger.error("Неверная длина номера карты. Ожидалось 16 символов.")
         raise ValueError("Неверная длина номера карты. Ожидалось 16 символов.")
+
     masks = card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+    logger.debug(f"Успешно замаскирован номер карты: {masks}")
     return masks
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
     if len(account_number) != 20:
+        logger.error("Неверная длина номера счета. Ожидалось 20 символов.")
         raise ValueError("Неверная длина номера счета. Ожидалось 20 символов.")
+
     masks = "**" + account_number[-4:]
+    logger.debug(f"Успешно замаскирован номер счета: {masks}")
     return masks
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,47 @@
 import json
-from typing import Dict, List
+import logging
+import os
+from typing import Any, Dict, List
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+
+os.makedirs("logs", exist_ok=True)
 
 
-def load_transactions(filepath: str) -> List[Dict]:
+file_handler = logging.FileHandler("logs/utils.log", mode="w", encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+
+
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+file_handler.setFormatter(file_formatter)
+
+
+if not logger.handlers:
+    logger.addHandler(file_handler)
+    logger.propagate = False
+
+
+def load_transactions(filepath: str) -> List[Dict[str, Any]]:
     """
     Загружает список транзакций из JSON-файла.
-    param filepath: путь до JSON-файла
-    return: список словарей с транзакциями или пустой список
+    :param filepath: путь до JSON-файла
+    :return: список словарей с транзакциями или пустой список
     """
     try:
         with open(filepath, "r", encoding="utf-8") as f:
             data = json.load(f)
             if isinstance(data, list):
+                logger.debug(f"Файл успешно загружен: {filepath}, количество записей: {len(data)}")
                 return data
-    except (FileNotFoundError, json.JSONDecodeError):
-        pass
+            else:
+                logger.error(f"Ожидался список в файле {filepath}, но получено: {type(data)}")
+    except FileNotFoundError:
+        logger.error(f"Файл не найден: {filepath}")
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON в файле: {filepath}")
+    except Exception as e:
+        logger.exception(f"Непредвиденная ошибка при загрузке файла {filepath}: {e}")
+
+    logger.debug(f"Возвращен пустой список транзакций из {filepath}")
     return []
